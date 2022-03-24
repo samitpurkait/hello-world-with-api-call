7582230f89b914097ea563830969a1e37ee71b1c
Samit Purkait <samit.purkait@gm.com>
2022-03-23 20:33:11 -0500

    -change ui, add push and get calls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
+interface todoItem {
+  id: number,
+  description: string,
+  details: string,
+  done: boolean
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,15 +15,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hello-world-with-api-call';
-  apiUrl = '';
-  result = '';
+  apiUrl = 'https://spring-boot-complete-1642649420913.azurewebsites.net';
+  result: todoItem[] = [];
+  todo = {
+    id: null,
+    description : '',
+    details : '',
+    done : null
+  }
 
   constructor(private readonly http: HttpClient) {}
 
   public callApi(){
-    this.http.get<string>(this.apiUrl).subscribe(data => {
+    this.http.get<todoItem[]>(this.apiUrl + '/getTodo').subscribe(data => {
       this.result = data;
       }
     );
+  }
+
+  public pushToApi(){
+    this.http.post<todoItem>(this.apiUrl + '/postTodo', this.todo).subscribe(data => {
+      this.callApi();
+      this.todo.id = null;
+      this.todo.details = '';
+      this.todo.description = '';
+      this.todo.done = null;
+      console.log('Success');
+    })
   }
 }
